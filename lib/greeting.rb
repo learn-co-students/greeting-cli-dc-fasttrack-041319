7bf7bcc1a39e97a597c23c1777ea28c906e466a7
Athena Salomons <athena.asalomons@gmail.com>
2019-04-13 19:10:09 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,15 @@
 # code the #greeting method here!
+
+ruby lib/greeting.rb
+
+def greeting(name)
+  "Hello #{name}. It's nice to meet you."
+end
+
+ruby spec/02_cli_spec.rb
+
+ruby bin/greet 
+
+def other_greeting
+  puts "Hi! I'm HAL, what's your name?"
+  gets.strip <chomp> = name
